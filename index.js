7cc4d147063545d4385743832a64e558dd379594
annekarolle <102996739+annekarolle@users.noreply.github.com>
2022-08-17 14:28:50 -0300

    feat/estilização e implmentando outros items

---
+++ index.js
@@ -0,0 +1,7 @@
+function openForm() {
+    document.getElementById("div1").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("div1").style.display = "none";
+  }
---
+++ store.js
@@ -0,0 +1,12 @@
+{/* <div class="container-produtos">
+<img src="./img/Ativo 144.png" alt="" width="100%">
+<p>Blusa Trick girl - longa rosa</p>
+<h3>R$ 49,90</h3>
+<a href="#">Adicione ao carrinho</a>
+</div> */}
+
+const containerProdutos = document.createElement("div")
+containerProdutos.classList.add("container-produtos")
+const imagem = document.createElement("img")
+const botaoAdicionar = document.createElement("button")
+botaoAdicionar.classList.add("botao-adicionar")
